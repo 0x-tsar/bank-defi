90f7354d507d0311cdf3c8b052cdc0235216692f
Gustavo <contato.gustavofernandes@hotmail.com>
2022-01-30 12:16:45 -0300

    implementing network and balances

--- src/ethereum.jsx
+++ src/ethereum.jsx
@@ -2,9 +2,22 @@ import { ethers } from "ethers";
 import Bank from "./build/Bank";
 
 export async function connectEthereum() {
-  const account = await window.ethereum.request({ method: "eth_accounts" });
+  const [account] = await window.ethereum.request({ method: "eth_accounts" });
   const netId = await window.ethereum.request({ method: "net_version" });
-  return [account, netId];
-}
 
-// export default connectEthereum();
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+  const contract = new ethers.Contract(
+    Bank.networks["5777"].address,
+    Bank.abi,
+    signer
+  );
+
+  const balance = await provider.getBalance(account);
+
+  // contract.once("ANYMETHOD", () => {
+  // do something
+  // });
+
+  return [account, netId, contract, balance];
+}
